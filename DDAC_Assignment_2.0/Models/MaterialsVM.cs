1643afad56f12398e21bece0475f4693358f5734
xueying <apple.khoo7886@gmail.com>
2021-07-30 21:53:43 +0800

    update MaterialsVM

---
+++ DDAC_Assignment_2.0/Models/MaterialsVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_Assignment_2._0.Models
+{
+    public class MaterialsVM
+    {
+        [Key]
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Please Select a Material")]
+        public string Material { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Please Select a Material")]
+        public string ImageName { get; set; }
+    }
+}
